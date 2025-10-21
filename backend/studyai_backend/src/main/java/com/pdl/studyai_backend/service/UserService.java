b94b84ccdb210d3e2662fb41d601e567e235fe0c
fsossa <fulbsossa16@gmail.com>
2025-10-21 16:22:37 +0200

    Feat: Update login - add password hash with bcrypt

--- backend/studyai_backend/src/main/java/com/pdl/studyai_backend/service/UserService.java
+++ backend/studyai_backend/src/main/java/com/pdl/studyai_backend/service/UserService.java
@@ -1,36 +1,41 @@
 package com.pdl.studyai_backend.service;
-
 import java.util.List;
 import java.util.Optional;
-
 import org.springframework.stereotype.Service;
-
 import com.pdl.studyai_backend.model.User;
 import com.pdl.studyai_backend.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 
 @Service
 public class UserService {
 
-    private final UserRepository repo;
+    @Autowired
+    private final UserRepository userRepository;
 
-    public UserService(UserRepository repo) {
-        this.repo = repo;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
     }
 
     public User create(User user) {
-        return repo.save(user);
+        // Hachage du mot de passe avant de le sauvegarder
+        // user.setPassword(passwordEncoder.encode(user.getPassword()));
+        return userRepository.save(user);
     }
 
     public Optional<User> findByEmail(String email) {
-        return repo.findByEmail(email);
+        return userRepository.findByEmail(email);
     }
 
     public List<User> findAll() {
-        return repo.findAll();
+        return userRepository.findAll();
     }
 
     public boolean existsByEmail(String email) {
-        return repo.existsByEmail(email);
+        return userRepository.existsByEmail(email);
     }
 }

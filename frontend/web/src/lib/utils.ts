7c3179b39842291be48632a12827f109c86c75d0
marlenegohi <angemarlenegohi@gmail.com>
2025-10-21 16:32:36 +0200

    Revert "feat : upload page, rebase"
    This reverts commit bbb2133e48bc0a31aa155dbe8a934ecb9b9df615.

--- frontend/web/src/lib/utils.ts
+++ frontend/web/src/lib/utils.ts
@@ -2,8 +2,3 @@
 export function cn(...classes: Array<string | false | null | undefined>) {
     return classes.filter(Boolean).join(" ");
 }
-
-
-export function generateUniqueId(prefix = ''): string {
-    return prefix + Math.random().toString(36).slice(2, 11);
-}

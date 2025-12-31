140867be4c963085ef770225db62bb77fcd6dbfe
Bio Paul Kobena <biokobena07@gmail.com>
2025-12-31 02:43:40 +0100

    feat: loading + pdf upload + choose ok

--- frontend/mobile/app/(auth)/login.tsx
+++ frontend/mobile/app/(auth)/login.tsx
@@ -28,6 +28,10 @@ const LoginScreen = () => {
   const handleRegister = () => {
     router.push("/(auth)/register")
   }
+
+  const handleHome = () => {
+    router.push("/home")
+  }
   return (
     <SafeAreaView style={styles.container}>
       <StatusBar style='dark' animated />
@@ -75,7 +79,7 @@ const LoginScreen = () => {
         <TouchableOpacity
           style={styles.loginButton}
           activeOpacity={0.8}
-          onPress={() => handleLogin}
+          onPress={handleHome}
         >
           <Text style={styles.loginButtonText}>Connexion</Text>
         </TouchableOpacity>
